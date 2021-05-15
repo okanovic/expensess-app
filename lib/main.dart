2f0f16fae6451ace92fe77ec3a19af355e5071ac
Okan TOPER <okan.toper94@gmail.com>
2021-05-15 23:31:09 +0300

    First style configuration completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,9 +8,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -26,6 +31,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "p1", title: "Head Phone", amount: 300, date: DateTime.now()),
+    Transaction(id: "p2", title: "Keyboard", amount: 370, date: DateTime.now())
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,10 +54,37 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             elevation: 5,
           ),
-          Card(
-            color: Colors.deepOrange,
-            child: Text("LIST OF TX!"),
-            
+          Column(
+            children: transactions.map((e) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 15.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green, width: 2),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        '₺' + e.amount.toString(),
+                        style: TextStyle(
+                            color: Colors.grey[600],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(e.title,style: TextStyle(color: Colors.black,fontSize: 14,fontWeight: FontWeight.bold),),
+                        Text(DateFormat('EEE, d/M/y').format(e.date),style: TextStyle(color: Colors.black,fontSize: 12))
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
           )
         ],
       ),
